be4ea4a514a41fa485eb71aceedefa218f2979e3
MarcosRFlores <marcosrflores737@gmail.com>
2025-02-26 20:14:24 -0300

    Archivos BD2, Estru, Ciber

---
+++ Semestre 7/Sistemas Distribuidos/Tarea Unidad 6/sd-master/lab7-transacciones/target/generated-sources/annotations/sd/transacciones/model/Persona_.java
@@ -0,0 +1,16 @@
+package sd.transacciones.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2025-02-26T17:28:02")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, Integer> cedula;
+    public static volatile SingularAttribute<Persona, String> apellido;
+    public static volatile SingularAttribute<Persona, Double> saldo;
+    public static volatile SingularAttribute<Persona, String> nombre;
+
+}
